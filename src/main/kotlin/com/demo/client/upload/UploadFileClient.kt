ef1cdd053cac39eadf526ad90ad01532f290bf0c
João Victor Albuquerque <albjoaov@gmail.com>
2021-01-07 17:21:01 -0300

    feat (main): [wip] add upload file with client streaming

--- src/main/kotlin/com/demo/client/upload/UploadFileClient.kt
+++ src/main/kotlin/com/demo/client/upload/UploadFileClient.kt
@@ -1,10 +1,46 @@
 package com.demo.client.upload
 
+import com.demo.upload.UploadFileRequest
 import com.demo.upload.UploadFileServiceGrpcKt
+import com.google.protobuf.ByteString
 import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 
-fun main() {
+suspend fun main() {
     val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build()
     val stub = UploadFileServiceGrpcKt.UploadFileServiceCoroutineStub(channel)
-    // stub.upload()
+
+    // A origem do arquivo é diversa e mudará de acordo com a necessidade
+    val path = Paths.get("src/main/resources/input/java_input.pdf")
+    val inputStream = Files.newInputStream(path)
+
+    val fileChunksFlow = createUploadRequestFlow(inputStream)
+    stub.upload(fileChunksFlow)
+
+    channel.shutdown()
+}
+
+fun createUploadRequestFlow(inputStream: InputStream): Flow<UploadFileRequest> = flow {
+
+    val metadata = UploadFileRequest.newBuilder()
+            .fileMetadataBuilder
+            .setFileName("java_input")
+            .setFileType("pdf")
+            .build()
+    val requestWithMetadata = UploadFileRequest.newBuilder().setFileMetadata(metadata).build()
+    emit(requestWithMetadata)
+
+    val byteArray = ByteArray(4096)
+    var readFileSize = inputStream.read(byteArray)
+    while (readFileSize > 0) {
+        val fileChunkData = ByteString.copyFrom(byteArray)
+        val request = UploadFileRequest.newBuilder().setFileChunkData(fileChunkData).build()
+        emit(request)
+        readFileSize = inputStream.read(byteArray)
+    }
+
 }
--- src/main/kotlin/com/demo/server/upload/UploadFileEndpoint.kt
+++ src/main/kotlin/com/demo/server/upload/UploadFileEndpoint.kt
@@ -1,20 +1,53 @@
 package com.demo.server.upload
 
+import com.demo.service.S3Service
 import com.demo.upload.UploadFileRequest
 import com.demo.upload.UploadFileResponse
-import com.demo.upload.UploadFileServiceGrpcKt
+import com.demo.upload.UploadFileServiceGrpcKt.UploadFileServiceCoroutineImplBase
 import com.demo.upload.UploadStatus
-import java.nio.file.Paths
-import javax.inject.Singleton
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.nio.ByteBuffer
+import javax.inject.Singleton
 
 @Singleton
-class UploadFileEndpoint : UploadFileServiceGrpcKt.UploadFileServiceCoroutineImplBase() {
+class UploadFileEndpoint(private val s3Service: S3Service) : UploadFileServiceCoroutineImplBase() {
 
-    val SERVER_BASE_PATH = Paths.get("src/main/resources/output")
+    private val LOGGER: Logger = LoggerFactory.getLogger(UploadFileEndpoint::class.java)
 
     override suspend fun upload(requests: Flow<UploadFileRequest>): UploadFileResponse {
-        return UploadFileResponse.newBuilder().setName("teste").setStatus(UploadStatus.SUCCESS).build();
+
+        /*
+            Modificar a inicialização
+         */
+        var fileName = ""
+        var fileType = ""
+        var chunkAccumulator = ByteArray(4096)
+
+        requests.collect {
+
+            if (it.hasFileMetadata()) {
+                LOGGER.info("File info received")
+                fileName = it.fileMetadata.fileName
+                fileType = it.fileMetadata.fileType
+            }
+
+            val chunkReceived = it.fileChunkData.toByteArray()
+            val newCapacity = chunkAccumulator.size + chunkReceived.size
+            chunkAccumulator = ByteBuffer.allocate(newCapacity)
+                                            .put(chunkAccumulator)
+                                            .put(chunkReceived)
+                                            .array()
+
+
+        }
+
+        val fileUrl = this.s3Service.storeFile(fileName, fileType, chunkAccumulator)
+
+        return UploadFileResponse.newBuilder().setStatus(UploadStatus.SUCCESS).setFileUrl(fileUrl).build()
     }
+
 
 }
